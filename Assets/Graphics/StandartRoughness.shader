83ce9f05ce925d309c0cacc6ec4575473456f22c
rbnelr <rbnelr@gmx.de>
2019-10-08 16:39:36 +0200

    potato mode, via script experiment

--- Assets/Graphics/StandartRoughness.shader
+++ Assets/Graphics/StandartRoughness.shader
@@ -19,7 +19,7 @@ Shader "Custom/StandartRoughness" {
         // Use shader model 3.0 target, to get nicer looking lighting
         #pragma target 3.0
 		
-//#pragma multi_compile __ POTATO_MODE
+		//#pragma multi_compile __ POTATO_MODE
 
         sampler2D _MainTex;
 
--- Assets/SettingsManager.cs
+++ Assets/SettingsManager.cs
@@ -1,37 +1,75 @@
 ﻿using UnityEngine;
 using Unity.Mathematics;
 using static Unity.Mathematics.math;
+using System.Collections.Generic;
+using System.Linq;
 
 public class SettingsManager : MonoBehaviour {
-	[System.Serializable]
-	public class GraphicsSettings {
+	[Header("Graphics Settings")]
 		
-		[Header("Graphics Settings")]
+	public bool VSync = false;
+
+	[Tooltip("-1 = no target (unlimited on pc)")]
+	public int TargetFramerate = 200;
+
+	public bool PotatoMode = false;
+
+	bool prevVSync = false;
+	int prevTargetFramerate = 200;
+	bool prevPotatoMode = false;
+	
+	void Start () {
+		GetMaterials();
+
+		QualitySettings.vSyncCount = VSync ? 1 : 0;
+		Application.targetFrameRate = TargetFramerate;
+		SetPotatoMode(PotatoMode);
 		
-		public bool VSync = false;
+		prevVSync = VSync;
+		prevTargetFramerate = TargetFramerate;
+		prevPotatoMode = PotatoMode;
+	}
+	void Update () {
+		if (VSync != prevVSync)
+			QualitySettings.vSyncCount = VSync ? 1 : 0;
+		if (TargetFramerate != prevTargetFramerate)
+			Application.targetFrameRate = TargetFramerate;
+		if (PotatoMode != prevPotatoMode)
+			SetPotatoMode(PotatoMode);
 
-		[Tooltip("-1 = no target (unlimited on pc)")]
-		public int TargetFramerate = 200; // 
+		prevVSync = VSync;
+		prevTargetFramerate = TargetFramerate;
+		prevPotatoMode = PotatoMode;
+	}
 
-		public void Update (GraphicsSettings prev=null) {
-			if (prev == null || prev.VSync != VSync)
-				QualitySettings.vSyncCount = VSync ? 1 : 0;
-			if (prev != null) prev.VSync = VSync;
-			
-			if (prev == null || prev.TargetFramerate != TargetFramerate)
-				Application.targetFrameRate = TargetFramerate;
-			if (prev != null) prev.TargetFramerate = TargetFramerate;
+	void OnDestroy () {
+		ResetPotatoMode();
+	}
 
+	Material[] mats;
+	Shader standardShader, potatoShader, standartRoughnessShader;
+
+	Dictionary<Material, Shader> originalShaders = new Dictionary<Material, Shader>();
+
+	void GetMaterials () {
+		mats = Resources.LoadAll<Material>("Asset");
+		standardShader = Shader.Find("Standard");
+		potatoShader = Shader.Find("Custom/PotatoModeShader");
+		standartRoughnessShader = Shader.Find("Custom/StandartRoughness");
+
+		originalShaders = mats.ToDictionary(x => x, x => x.shader);
+	}
+
+	void SetPotatoMode (bool on) {
+		foreach (var m in originalShaders) {
+			m.Key.shader = on ? potatoShader : m.Value;
 		}
 	}
 
-	public GraphicsSettings Graphics = new GraphicsSettings();
-	GraphicsSettings prevGraphics = new GraphicsSettings();
 	
-	void Start () {
-		Graphics.Update();
-	}
-	void Update () {
-		Graphics.Update(prevGraphics);
+	void ResetPotatoMode () {
+		foreach (var m in originalShaders) {
+			m.Key.shader = m.Value;
+		}
 	}
 }
